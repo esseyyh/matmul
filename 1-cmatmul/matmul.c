e48bce40b823a368e62751cfb1c5bdfbeb6dbae8
Essey <essey@Nyx.Nyx>
2024-10-01 20:32:05 +0400

    naive c impl

--- 1-cmatmul/matmul.c
+++ 1-cmatmul/matmul.c
@@ -1,6 +1,76 @@
 #include <stdio.h>
-int main()
-{
+#include <stdlib.h>
+#include <time.h>
+
+
+void matmul(float *A,float *B,float *C, int i, int j , int k){
+
+    for (int ii=0;ii<i;ii++){
+
+        for (int jj=0;jj<j;jj++){
+            float value=0;
+            for (int kk=0;kk<k;kk++){
+                value= A[ii * k  + kk ]  * B [j * kk + jj ];
+            }
+            C[ii * j +  jj ]=value;
+
+        }
+    }
+
+
+
+}
+
+
+int main(){
+
+
+    int sizes [][3]= { {128,128,128}, {256,256,256}, {512,512,512}};
+    int iteration=10;
+
+    //printf("%d \n",(sizeof(sizes)/sizeof(sizes[0])));
+    srand(time(NULL));
+    printf("m,n,k,time,flops\n");
+    for (int i=0 ; i < sizeof(sizes)/sizeof(sizes[0]) ; i++){
+        int M = sizes[i][0];
+        int N = sizes[i][1];
+        int K = sizes[i][2];
+
+        float *A = (float *)malloc(M * K * sizeof(float));
+        float *B = (float *)malloc(K * N * sizeof(float));
+        float *C = (float *)malloc(M * N * sizeof(float));
+
+        for (int j = 0; j < M * K; j++) {
+            A[j] = (float)rand() / RAND_MAX;
+        }
+        for (int j = 0; j < K * N; j++) {
+            B[j] = (float)rand() / RAND_MAX;
+        }
+
+        
+        for (int iter = 0; iter < iteration; iter++) {
+            clock_t start_time = clock();
+            matmul(A,B,C, M, N,K);
+            clock_t end_time = clock();
+
+            double iteration_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+            double flops = 2.0 * M * N * K;
+            double flops_per_second = flops / iteration_time / 1e9;  // Convert to gigaflops
+
+            printf("%d,%d,%d,%.6f,%.2f\n", M, N, K, iteration_time, flops_per_second);
+        }
+
+
+
+    
+      
+        free(A);
+        free(B);
+        free(C);
+
+    }
+    return 0;
+
 
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # matmul
+
+
+## CPU implement 
+1. naive c matmul
+2. strassen algorithm
+3. single thread optim
+    tiling
+    transpose
+    vetctoriztion
+    prefetch 
+    unrolling
+4. and finally multi threading
+
+## GPU Implementation 
+
+1. naive
+2. tiling
+
+
+
+
+
+
